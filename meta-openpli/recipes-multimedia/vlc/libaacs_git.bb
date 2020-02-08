OpenPLi/openpli-oe-core

videolan moved to gitlab

meta-openpli/recipes-multimedia/vlc/libaacs_git.bb
@@ -6,7 +6,7 @@ DEPENDS = "libgcrypt"
 PV = "0.3.0+git${SRCPV}"
 PR = "r1"
 
-SRC_URI = "git://git.videolan.org/${PN}.git;protocol=git"
+SRC_URI = "git://code.videolan.org/videolan/${PN}.git;protocol=http"
 
 S = "${WORKDIR}/git"
 

satdreamgr/oe-core

Conflicts:
	meta-openpli/recipes-multimedia/vlc/libaacs_git.bb

meta-openpli/recipes-multimedia/vlc/libaacs_git.bb
@@ -6,7 +6,7 @@ DEPENDS = "libgcrypt"
 PV = "0.3.0+git${SRCPV}"
 PR = "r1"
 
-SRC_URI = "git://git.videolan.org/${BPN}.git;protocol=git"
+SRC_URI = "git://code.videolan.org/videolan/${BPN}.git;protocol=http"
 
 S = "${WORKDIR}/git"
 